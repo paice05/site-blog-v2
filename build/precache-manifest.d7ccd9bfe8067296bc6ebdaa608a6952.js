3f3fdc2b7410b71a9ad489b36e454beb303d4647
Matheus Paice <matheus.paice@gmail.com>
2020-02-23 12:42:50 -0300

    build

---
+++ build/precache-manifest.d7ccd9bfe8067296bc6ebdaa608a6952.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1183e10c4b151e7fb286b4a829038b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309c3fe902ba8ad6a524",
+    "url": "/static/css/main.54c18f54.chunk.css"
+  },
+  {
+    "revision": "2811b8b06f010e4b6cb1",
+    "url": "/static/js/2.ca0a69d5.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.ca0a69d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309c3fe902ba8ad6a524",
+    "url": "/static/js/main.497492d9.chunk.js"
+  },
+  {
+    "revision": "638363bbbd133fd9250c",
+    "url": "/static/js/runtime-main.db98aebd.js"
+  },
+  {
+    "revision": "ffb7f8240b22613e433742d2ee3f2435",
+    "url": "/static/media/img1.ffb7f824.jpg"
+  },
+  {
+    "revision": "4654195d8c9b71a11fddfbf05485326b",
+    "url": "/static/media/img2.4654195d.jpg"
+  },
+  {
+    "revision": "020631bfa12b587ffe7afdb7badd6543",
+    "url": "/static/media/img3.020631bf.jpg"
+  },
+  {
+    "revision": "134fdf301fae0884d81ea256e37287ba",
+    "url": "/static/media/img4.134fdf30.jpg"
+  },
+  {
+    "revision": "d9d94902f8e9d6de1f88185217ba2263",
+    "url": "/static/media/img5.d9d94902.jpg"
+  }
+]);
